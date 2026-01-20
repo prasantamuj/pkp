941a6b4e1ef4c19de013d5f692f477f92e4b4133
prasantapaul <kumar.prasantapaul@gmail.com>
2026-01-20 19:15:39 +0530

    map details

---
+++ StreamAPI/src/test/java/org/example/mapStream.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.stream.*;
+public class mapStream {
+    //Using Stream map(Function mapper) to
+    //convert the Strings in stream to
+    //UpperCase form
+
+    public static void main(String[] args) {
+        Stream<Integer> streamInt = Stream.of(3,4,5,6,7,8,9);
+        Stream<String> streamString = Stream.of("vivaan", "dhrub","sonu","lalita");
+        Stream<String> streamString2 = Stream.of("vivaan", "dhrub","sonu","lalita");
+        streamInt.map(i-> i*4).forEach(e-> System.out.println(e));
+        streamString.map(str->str.toUpperCase()).forEach(System.out::println);
+        streamString2.map(str->str.length()).sorted((a,b)->a-b).forEach(System.out::println);
+    }
+}
